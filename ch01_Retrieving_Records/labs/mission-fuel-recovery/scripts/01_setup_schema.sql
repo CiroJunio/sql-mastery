9a61693d779ee73d15f0905932db8d7954cc360d
Ciro J <cirojunioteixeira1@gmail.com>
2026-04-19 14:14:47 -0300

    fix(labs/mission-fuel-recovery): Fixes SQL and ensures Docker is working properly.

---
+++ ch01_Retrieving_Records/labs/mission-fuel-recovery/scripts/01_setup_schema.sql
@@ -0,0 +1,12 @@
+-- 1. SETUP DO AMBIENTE (O Pântano)
+CREATE TABLE truck_telemetry (
+    truck_id INTEGER PRIMARY KEY,
+    driver_name VARCHAR(50),
+    fuel_level INTEGER,          
+    battery_voltage DECIMAL(4,2), 
+    temp_cabin INTEGER,          
+    tire_pressure_1 INTEGER,     
+    tire_pressure_2 INTEGER,     
+    last_oil_change DATE,        
+    extra_sensor_data TEXT       
+);
---
+++ ch01_Retrieving_Records/labs/mission-fuel-recovery/scripts/02_seed_data.sql.sql
@@ -0,0 +1,14 @@
+INSERT INTO truck_telemetry (truck_id, driver_name, fuel_level, battery_voltage, temp_cabin, extra_sensor_data)
+VALUES 
+(101, 'Alpha', 50, 13.5, 22, 'Data_Log_7721'),
+(102, 'Bravo', NULL, 12.1, 24, 'Data_Log_8812'), 
+(103, 'Charlie', 5, NULL, 21, 'Data_Log_9901'),  
+(104, 'Delta', 80, 14.0, 25, 'Data_Log_1102'),
+(105, 'Echo', 10, 11.5, 23, 'Data_Log_3341'),
+(106, 'Foxtrot', NULL, NULL, 22, 'Data_Log_5521'), 
+(107, 'Golf', 15, 12.0, 24, 'Data_Log_6672'),
+(108, 'Hotel', 2, 10.5, 21, 'Data_Log_2210'),
+(109, 'India', 60, 13.8, 26, 'Data_Log_0091'),
+(110, 'Juliet', 12, 11.2, 22, 'Data_Log_1182'),
+(111, 'Kilo', NULL, 11.0, 23, 'Data_Log_4453'),
+(112, 'Lima', 5, 10.8, 24, 'Data_Log_9982');
