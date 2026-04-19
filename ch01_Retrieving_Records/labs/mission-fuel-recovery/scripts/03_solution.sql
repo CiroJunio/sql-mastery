1babdaeb77319e8e0add4d78877f81d0eac1a86e
Ciro J <cirojunioteixeira1@gmail.com>
2026-04-19 19:27:58 -0300

    fix(labs\misison-fuel-recovery): Select the fuel_level column correctly.

--- ch01_Retrieving_Records/labs/mission-fuel-recovery/scripts/03_solution.sql
+++ ch01_Retrieving_Records/labs/mission-fuel-recovery/scripts/03_solution.sql
@@ -1,7 +1,14 @@
-SELECT truck_id, driver_name, COALESCE(fuel_level, 0) AS fuel_level, 
-                              COALESCE(battery_voltage, 10) AS battery_voltage
-FROM truck_telemetry limit 5
-where fuel_level <= 20
+SELECT truck_id,  
+       COALESCE(fuel_level, 0) AS fuel_level, 
+       COALESCE(battery_voltage, 10) AS battery_voltage,
+    CASE 
+        WHEN fuel_level < 15 THEN 'CRITICAL'
+        WHEN fuel_level > 15 THEN 'MONITORING'
+        ELSE 'OK'
+    END AS SCORE
+FROM truck_telemetry 
+where COALESCE(fuel_level, 0) <= 20
+LIMIT 5;
 
 
 SELECT *
